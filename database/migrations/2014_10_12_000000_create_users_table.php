5a1c6c25b70007188c62b071f75586a6dc25729f
Annei <edelyav@gmail.com>
2019-07-04 13:33:37 -0500

    Expedientes Digitales

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrador', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('usuario');
+            $table->string('apellidop');
+            $table->string('apellidom');
+            $table->string('nombre');
+            $table->string('celular')->unique();
+            
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administrador');
+    }
+}
